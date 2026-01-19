0944e8d20942f4fc5d588d4d12dcb955eccdf6fa
Rory Self <rory.self@protonmail.com>
2026-01-19 11:37:33 +1100

    Updated clangd configuration to use C++23 by default

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -2,4 +2,7 @@ return {
 	cmd = { 'clangd' },
 	filetypes = { 'c', 'cpp' },
 	root_markers = { '.git', 'compile_commands.json', 'compile_flags.txt' },
+  init_options = {
+    fallbackFlags = { "-std=c++23" },
+  },
 }
