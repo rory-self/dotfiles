638ba596bc5cc7b896b2de4fa6fa7fa023964d13
Rory Self <rory.self@protonmail.com>
2026-01-19 11:41:56 +1100

    Fixed indentation misalignment in clangd config

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,7 +1,7 @@
 return {
-	cmd = { 'clangd' },
-	filetypes = { 'c', 'cpp' },
-	root_markers = { '.git', 'compile_commands.json', 'compile_flags.txt' },
+  cmd = { 'clangd' },
+  filetypes = { 'c', 'cpp' },
+  root_markers = { '.git', 'compile_commands.json', 'compile_flags.txt' },
   init_options = {
     fallbackFlags = { "-std=c++23" },
   },
